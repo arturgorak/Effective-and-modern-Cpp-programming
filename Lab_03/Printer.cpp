667db6a08ec8aa65b9ea43b761a47a41eacf07b9
arturgorak <arturgorak@gmail.com>
2022-03-20 20:17:03 +0100

    Adding a solution to the exercise in lab_03

---
+++ Lab_03/Printer.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <vector>
+#include <fstream>
+#include <algorithm>
+
+using namespace std;
+class Printer{
+    std::ostream& out;
+    std::string prefix;
+    std::string postfix;
+
+public:
+    Printer(std::ostream& out, std::string prefix, std::string postfix) : out(out), prefix(std::move(prefix)), postfix(std::move(postfix)) {};
+
+    template<typename T>
+    void operator()(T t) const {
+        out << prefix << t << postfix;
+    };
+};
+/*
+int main(){
+  /// Creates unary functor that takes one argument x (of any type)
+  /// and outputs to given stream x surrounded by given prefix na postfix
+  /// e.g. in the following  [ x ]
+  /// Hint: define function template.
+  Printer printer(std::cout,  "[ ", " ] " );
+  printer("hello");    // [ hello ]
+  std::cout << "\nv = ";
+  std::vector<int> v = {1, 2, 3, 4};
+  std::for_each(v.begin(), v.end(), printer);  // v = [ 1 ] [ 2 ] [ 3 ] [ 4 ]
+
+  std::ofstream file("myFile.txt");
+  Printer filePrinter(file, "- ", "\n");
+  filePrinter(5);
+  filePrinter("My text");
+	return 0;
+}
+ */
+/** myFile.txt
+- 5
+- My text
+*/
---
+++ Lab_03/function_pointers.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+/// Computes n-th root of x
+double sqrtn(int n, double x){
+	return exp( log(x) / n); 
+}
+double power(double  x, double y){
+	return exp( log(x) * y);
+}
+template <typename F, typename G>
+double function(int n, double y, double z, F f, G g){
+	return (f(n,y) > z) ? g(z, y) : g(y, z);
+}
+//int main(){
+//	//auto fp1 = sqrtn;
+//    double (*fp1)(int, double ) = sqrtn;
+//    //auto fp2 = power;
+//    double (*fp2)(double, double ) = power;
+//	//auto fp3 = function<decltype(fp1), decltype(fp2)>;
+//    double (*fp3)(int, double, double, double (*)(int, double),double (*)(double, double)) = function<double(*)(int, double ), double(*)(double, double )>;
+//	cout << fp3(2, 10, 3, fp1, fp2) << endl;
+//	cout << fp3(3, 10, 3, fp1, fp2) << endl;
+//	return 0;
+//}
+
+//59049
+//1000
